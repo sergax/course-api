9a96d2f1d55423537100494705cf4fba6a7e6187
user <user@pop-os.localdomain>
2022-09-14 16:07:27 +0300

    add CourseRestControllerV1 Test

---
+++ src/test/java/com/sergax/courseapi/controller/CourseRestControllerV1Test.java
@@ -0,0 +1,76 @@
+package com.sergax.courseapi.controller;
+
+import com.sergax.courseapi.dto.ContentDto;
+import com.sergax.courseapi.dto.CourseDto;
+import com.sergax.courseapi.model.user.User;
+import com.sergax.courseapi.service.ContentService;
+import com.sergax.courseapi.service.CourseService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.security.Principal;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CourseRestControllerV1Test {
+    @Mock
+    private CourseService courseServiceMock;
+    @Mock
+    private ContentService contentService;
+    @InjectMocks
+    private CourseRestControllerV1 courseRestControllerV1Test;
+    private final Principal principal = mock(Principal.class);
+    private final User mentorTest = new User();
+    private final CourseDto courseDtoTest = new CourseDto();
+    private final ContentDto contentDtoTest = new ContentDto();
+
+    @BeforeEach
+    void setUp() {
+        mentorTest.setEmail("mentorTest@mail.com");
+        Mockito.when(principal.getName()).thenReturn(mentorTest.getEmail());
+    }
+
+    @Test
+    void contextLoads() {
+        assertThat(courseRestControllerV1Test).isNotNull();
+    }
+
+    @Test
+    void findAllCourses() {
+        courseRestControllerV1Test.findAllCourses();
+        verify(courseServiceMock).findAll();
+    }
+
+    @Test
+    void findUserById() {
+        courseRestControllerV1Test.findUserById(anyLong());
+        verify(courseServiceMock).findById(anyLong());
+    }
+
+    @Test
+    void createCourseByMentor() {
+        courseRestControllerV1Test.createCourseByMentor(courseDtoTest, principal);
+        verify(courseServiceMock).createCourseByMentor(courseDtoTest, mentorTest.getEmail());
+    }
+
+    @Test
+    void addContentToCourse() {
+        courseRestControllerV1Test.addContentToCourse(1L, contentDtoTest, principal);
+        verify(contentService).addContentToCourse(1L, contentDtoTest, mentorTest.getEmail());
+    }
+
+    @Test
+    void updateUser() {
+        courseRestControllerV1Test.updateUser(1L, courseDtoTest);
+        verify(courseServiceMock).update(1L, courseDtoTest);
+    }
+}
--- src/test/java/com/sergax/courseapi/controller/UserRestControllerV1Test.java
+++ src/test/java/com/sergax/courseapi/controller/UserRestControllerV1Test.java
@@ -1,13 +1,14 @@
 package com.sergax.courseapi.controller;
 
 import com.sergax.courseapi.service.UserService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.verify;
@@ -17,41 +18,46 @@ class UserRestControllerV1Test {
     @Mock
     private UserService userServiceMock;
     @InjectMocks
-    private UserRestControllerV1 userRestControllerV1;
+    private UserRestControllerV1 userRestControllerV1Test;
+
+    @Test
+    void contextLoads() {
+        assertThat(userRestControllerV1Test).isNotNull();
+    }
 
     @Test
     void findAllUsers() {
-        userRestControllerV1.findAllUsers();
+        userRestControllerV1Test.findAllUsers();
         verify(userServiceMock).findAll();
     }
 
     @Test
     void findUserById() {
-        userRestControllerV1.findUserById(1L);
+        userRestControllerV1Test.findUserById(1L);
         verify(userServiceMock).findById(1L);
     }
 
     @Test
     void createUser() {
-        userRestControllerV1.createUser(any());
+        userRestControllerV1Test.createUser(any());
         verify(userServiceMock).save(any());
     }
 
     @Test
     void updateUser() {
-        userRestControllerV1.updateUser(anyLong(), any());
+        userRestControllerV1Test.updateUser(anyLong(), any());
         verify(userServiceMock).update(anyLong(), any());
     }
 
     @Test
     void addRoleToUser() {
-        userRestControllerV1.addRoleToUser(anyLong(), anyLong());
+        userRestControllerV1Test.addRoleToUser(anyLong(), anyLong());
         verify(userServiceMock).addRoleForUserById(anyLong(), anyLong());
     }
 
     @Test
     void deleteUser() {
-        userRestControllerV1.deleteUser(anyLong());
+        userRestControllerV1Test.deleteUser(anyLong());
         verify(userServiceMock).deleteById(anyLong());
     }
 }
